8abf7c4d24f7b695aed9eee43c12ad8827f7b9c9
Felipevieira2 <felipy_erivaldo@hotmail.com>
2017-11-25 03:28:03 -0200

    deletando

--- pi/testes/logout.php
+++
@@ -1,9 +0,0 @@
-﻿<?php 
-session_start();
-session_destroy();
-header("Location: login.php");
-?>
-
-
-
-
--- pi2/menu/index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include('../db/conexao.php');
-include('../auth/controle.php');
-include('users.php');
-?>
