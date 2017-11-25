18eb9d5380c1b6757122d37ade3c7b1b023048f9
Felipevieira2 <felipy_erivaldo@hotmail.com>
2017-11-25 04:14:23 -0200

    Update conexao.php

--- db/conexao.php
+++ db/conexao.php
@@ -3,7 +3,7 @@
 $dbHost= "serverf.database.windows.net";   #Nome do host
 $dbName= "db_felipe";   #Nome do banco de dados
 $dbUser= "felipe"; #Nome do usuário
-$dbPassword= "#######";   #Senha do usuário
+$dbPassword= "##########";   #Senha do usuário
 $dsn = "Driver={SQL Server};
 		Server=$dbHost;
 		Port=1433;
